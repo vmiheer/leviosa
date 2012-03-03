162d7d023ecf5210beab8a91959e3970797dabec
miheer vaidya <v.miheer@gmail.com>
2012-03-04 01:49:58 +0530

    Added authors

--- helper.c
+++ helper.c
@@ -1,3 +1,20 @@
+//    			libhelper [leviosa]
+//	A wrapper for XTest
+//  Copyright (C) 2012  
+//  miheer mukund vaidya
+//  v.miheer@gmail.com
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include"helper.h"
 Display *dpy;
 void helper_init()
@@ -8,7 +25,7 @@ void helper_init()
 	printf("Inside C\n");
 	XTestGrabControl(dpy,True);
 //	XTestFakeButtonEvent(dpy,1,True,CurrentTime);
-//	XTestFakeRelativeMotionEvent(dpy,1024,768,CurrentTime);
+	XTestFakeMotionEvent(dpy,-1,0,0,CurrentTime);
 //	XTestFakeButtonEvent(dpy,1,False,CurrentTime);
 	XSync(dpy,False);
 	XTestGrabControl(dpy,False);
@@ -38,4 +55,9 @@ void helper_press()
 	XTestGrabControl(dpy,False);
 }
 void helper_release()
-
+{
+	XTestGrabControl(dpy,True);
+	XTestFakeButtonEvent(dpy,1,False,CurrentTime);
+	XSync(dpy,False);
+	XTestGrabControl(dpy,False);
+}
